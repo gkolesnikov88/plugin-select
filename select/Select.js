5bc285ff18fa333cd3076a1480f17ff3b88afaeb
George Kolesnikov <gkolesnikov88@gmail.com>
2023-11-19 13:26:44 +0300

    Highlight selectem item in the list

--- select/Select.js
+++ select/Select.js
@@ -66,6 +66,9 @@ export class Select {
     select(id) {
         this.selectedId = id;
         this.$placeholderText.textContent = this.current.value;
+        this.$el.querySelectorAll(`[data-type="item"]`).forEach(el => 
+            el.classList.remove('selected'));
+        this.$el.querySelector(`[data-id="${id}"]`).classList.add('selected');
         this.close();
     }
 
